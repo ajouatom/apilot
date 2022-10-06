9dae94028be819e8ad485d27b27639c49cf4ddf6
ajouatom <ajouatom@gmail.com>
2022-10-06 19:03:32 +0900

    parser...

--- opendbc/can/parser.cc
+++ opendbc/can/parser.cc
@@ -287,7 +287,7 @@ void CANParser::UpdateValid(uint64_t sec) {
     const bool missing = state.last_seen_nanos == 0;
     const bool timed_out = (sec - state.last_seen_nanos) > state.check_threshold;
     if (state.check_threshold > 0 && (missing || timed_out)) {
-      if (0 && show_missing && !bus_timeout) {
+      if (show_missing && !bus_timeout) {
         if (missing) {
           LOGE("0x%X NOT SEEN", state.address);
         } else if (timed_out) {
