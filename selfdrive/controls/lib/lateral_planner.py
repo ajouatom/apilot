fdc473c067a354e9b767f342c3034d7435091db9
ajouatom <ajouatom@gmail.com>
2022-10-07 10:44:23 +0900

    update

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -75,7 +75,7 @@ class LateralPlanner:
                      y_pts,
                      heading_pts,
                      yaw_rate_pts)
-    # init state for next
+    # init state for next iteration
     # mpc.u_sol is the desired second derivative of psi given x0 curv state.
     # with x0[3] = measured_yaw_rate, this would be the actual desired yaw rate.
     # instead, interpolate x_sol so that x0[3] is the desired yaw rate for lat_control.
@@ -107,7 +107,7 @@ class LateralPlanner:
     lateralPlan.psis = self.lat_mpc.x_sol[0:CONTROL_N, 2].tolist()
 
     # clip speed for curv calculation at 1m/s, to prevent low speed extremes
-    clipped_speed = min(1.0, sm['carState'].vEgo)
+    clipped_speed = max(1.0, sm['carState'].vEgo)
     lateralPlan.curvatures = (self.lat_mpc.x_sol[0:CONTROL_N, 3]/clipped_speed).tolist()
     lateralPlan.curvatureRates = [float(x/clipped_speed) for x in self.lat_mpc.u_sol[0:CONTROL_N - 1]] + [0.0]
 
