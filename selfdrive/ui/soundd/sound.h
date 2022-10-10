6e7909c29a5bfd986100910de6a1b13c9523bb31
ajouatom <ajouatom@gmail.com>
2022-10-11 08:39:05 +0900

    fix..

--- selfdrive/ui/soundd/sound.h
+++ selfdrive/ui/soundd/sound.h
@@ -17,8 +17,8 @@ const std::tuple<AudibleAlert, QString, int> sound_list[] = {
 
   {AudibleAlert::WARNING_SOFT, "warning_soft.wav", QSoundEffect::Infinite},
   {AudibleAlert::WARNING_IMMEDIATE, "warning_immediate.wav", QSoundEffect::Infinite},
-  {AudibleAlert::LONG_ENGAGE, "tici_engaged.wav", 0},
-  {AudibleAlert::LONG_DISENGAGE, "tici_disengaged.wav", 0},
+  {AudibleAlert::LONG_ENGAGED, "tici_engaged.wav", 0},
+  {AudibleAlert::LONG_DISENGAGED, "tici_disengaged.wav", 0},
 };
 
 class Sound : public QObject {
