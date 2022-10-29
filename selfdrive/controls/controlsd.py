219141432da4fee0bd2a08987e8a220f36d100f1
ajouatom <ajouatom@gmail.com>
2022-10-29 11:50:47 +0900

    controls mismatch tmux 출력~

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -325,7 +325,7 @@ class Controls:
         safety_mismatch = pandaState.safetyModel not in IGNORED_SAFETY_MODES
 
       if safety_mismatch or pandaState.safetyRxChecksInvalid or self.mismatch_counter >= 200:
-        #print('mismatch = {},{},{}'.format(safety_mismatch, pandaState.safetyRxChecksInvalid, self.mismatch_counter)) 
+        print('mismatch = {},{},{}'.format(safety_mismatch, pandaState.safetyRxChecksInvalid, self.mismatch_counter)) 
         self.events.add(EventName.controlsMismatch)
 
       if log.PandaState.FaultType.relayMalfunction in pandaState.faults:
