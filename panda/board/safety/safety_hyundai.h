5d3ce390b51a6966101c0e1b9dea9ea8af4dd854
ajouatom <ajouatom@gmail.com>
2022-11-23 18:41:41 +0900

    fix..

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -316,7 +316,7 @@ static int hyundai_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
   }
 
   // UDS: Only tester present ("\x02\x3E\x80\x00\x00\x00\x00\x00") allowed on diagnostics address
-  if ((addr == 2000) && !hyundai_scc_bus2) {  // ajouatom
+  if (addr == 2000) {
     if ((GET_BYTES_04(to_send) != 0x00803E02U) || (GET_BYTES_48(to_send) != 0x0U)) {
       tx = 0;
     }
