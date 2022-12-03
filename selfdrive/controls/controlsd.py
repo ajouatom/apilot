f3861fb99d60b9921b4a5619f1aaefa8a67bd8d9
ajouatom <ajouatom@gmail.com>
2022-12-03 11:05:54 +0900

    debugText

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -597,7 +597,7 @@ class Controls:
       torque_params = self.sm['liveTorqueParameters']
       if self.sm.all_checks(['liveTorqueParameters']) and torque_params.useParams:
         self.LaC.update_live_torque_params(torque_params.latAccelFactorFiltered, torque_params.latAccelOffsetFiltered, torque_params.frictionCoefficientFiltered)
-        self.debugText2 = 'LiveT[{:.0f}{}]: {:.2f},{:.2f},{:.2f}\nRawT: {:.2f},{:.2f},{:.2f}'.format(torque_params.totalBucketPoints, torque_params.liveValid, 
+        self.debugText2 = "LiveT[{:.0f}{}]: {:.2f},{:.2f},{:.2f}\nRawT: {:.2f},{:.2f},{:.2f}".format(torque_params.totalBucketPoints, torque_params.liveValid, 
                                                                                               torque_params.latAccelFactorFiltered, torque_params.latAccelOffsetFiltered,torque_params.frictionCoefficientFiltered, 
                                                                                               torque_params.latAccelFactorRaw, torque_params.latAccelOffsetRaw, torque_params.frictionCoefficientRaw)
         #print(self.debugText2)
