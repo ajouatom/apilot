dc128c566ab95a9178cb636cc2de9adae8f088de
ajouatom <ajouatom@gmail.com>
2022-12-03 13:36:44 +0900

    롱컨선택항상나오도록함.

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -181,7 +181,7 @@ void TogglesPanel::updateToggles() {
     e2e_toggle->refresh();
   } else {
     e2e_toggle->setDescription(e2e_description);
-    op_long_toggle->setVisible(false);
+    op_long_toggle->setVisible(true);
   }
 }
 
