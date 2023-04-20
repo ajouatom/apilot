a2fef2dc577dada92a897dc3399afad553897807
ajouatom <ajouatom@gmail.com>
2023-04-20 18:20:57 +0900

    path폭..

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -200,6 +200,9 @@ public:
   float show_z_offset = 1.22;
   int show_path_mode = 0;
   int show_path_color = 14;
+  int show_path_mode_lane = 0;
+  int show_path_color_lane = 14;
+  float show_path_width = 1.0;
 
 signals:
   void uiUpdate(const UIState &s);
