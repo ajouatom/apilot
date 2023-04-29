3b6c4a9981ec0b368a623c60a5d0ba6586aa21f6
ajouatom <ajouatom@gmail.com>
2023-04-29 15:15:56 +0900

    SCC레이더값... vRel이 음수일때와 양수일때 다름..

--- selfdrive/car/hyundai/radar_interface.py
+++ selfdrive/car/hyundai/radar_interface.py
@@ -6,6 +6,7 @@ from opendbc.can.parser import CANParser
 from selfdrive.car.interfaces import RadarInterfaceBase
 from selfdrive.car.hyundai.values import DBC
 from common.params import Params
+from common.numpy_fast import interp
 
 RADAR_START_ADDR = 0x500
 RADAR_MSG_COUNT = 32
@@ -123,8 +124,10 @@ class RadarInterface(RadarInterfaceBase):
             self.pts[ii].trackId = self.track_id
             self.track_id += 1
 
+          vRel = cpt["SCC11"]['ACC_ObjRelSpd']
           #self.pts[ii].dRel = cpt["SCC11"]['ACC_ObjDist']  # from front of car
           self.pts[ii].dRel = (cpt["SCC11"]['ACC_ObjDist'] - 0.6) * 1.18 # from front of car  레이더트랙과 비슷하게... 조작
+          self.pts[ii].dRel = interp(vRel, [-1.0, 0.0], [cpt["SCC11"]['ACC_ObjDist'], (cpt["SCC11"]['ACC_ObjDist'] - 0.6) * 1.18])
           self.pts[ii].yRel = -cpt["SCC11"]['ACC_ObjLatPos']  # in car frame's y axis, left is negative
           self.pts[ii].vRel = cpt["SCC11"]['ACC_ObjRelSpd']
           self.pts[ii].aRel = float('nan')
