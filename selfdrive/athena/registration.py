5022898093bf4d0ac5e7c07065c06c7b0e8de1b8
Lotx-001 <72420846+Lotx-001@users.noreply.github.com>
2023-09-09 02:45:46 +0300

    Fix
    Fixed an issue connecting to the comma.ai warning message
    servers, and does not receive any support from comma.ai. If this is an official device, visit https://comma.ai/support.

--- selfdrive/athena/registration.py
+++ selfdrive/athena/registration.py
@@ -93,7 +93,7 @@ def register(show_spinner=False) -> Optional[str]:
 
   if dongle_id:
     params.put("DongleId", dongle_id)
-    set_offroad_alert("Offroad_UnofficialHardware", (dongle_id == UNREGISTERED_DONGLE_ID) and not PC)
+    set_offroad_alert("Offroad_UnofficialHardware", False)
   return dongle_id
 
 
