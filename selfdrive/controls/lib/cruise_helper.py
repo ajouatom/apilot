0fb74bbbca45b663541cd47ce4d75fa302dbc36b
ajouatom <ajouatom@gmail.com>
2023-09-18 16:54:30 +0900

    fix...

--- selfdrive/controls/lib/cruise_helper.py
+++ selfdrive/controls/lib/cruise_helper.py
@@ -595,11 +595,11 @@ class CruiseHelper:
           longActiveUser = -2
         #  2. 신호감지감속중: cruiseOFF: 신호감지감속이 맘에 안드는 상태, 가속페달을 밟으면 해제
         elif self.xState in [XState.e2eStop, XState.e2eCruise, XState.e2eCruisePrepare] and self.v_ego_kph < v_cruise_kph and (self.trafficState % 10) == 1: #controls.v_future*CV.MS_TO_KPH < v_ego_kph * 0.6: 
-          v_cruise_kph = self.v_ego_kph_set
+          #v_cruise_kph = self.v_ego_kph_set
           longActiveUser = -2
         #  3. 저속주행: cruiseOFF(autoResumeFromGasSpeed 이하): 조건(autoCancelFromGasMode)에 따라 선행차의 유무에 따라 크루즈 해제
         elif self.v_ego_kph < self.autoResumeFromGasSpeed:
-          v_cruise_kph = self.v_ego_kph_set
+          #v_cruise_kph = self.v_ego_kph_set
           if self.autoCancelFromGasMode == 1:
             longActiveUser = -2
           elif self.autoCancelFromGasMode == 2 and self.dRel==0: # mode:2일때는 선행차가 없을때만
