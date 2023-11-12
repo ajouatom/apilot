5467f3343163cbb7b382114ed97a068ad18e4f72
ajouatom <ajouatom@gmail.com>
2023-11-13 07:20:54 +0900

    fix.. disp

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1166,7 +1166,7 @@ void DrawApilot::drawLeadApilot(const UIState* s) {
         }
         else if (disp_dist > 0.0) {
             if (radar_dist < 10.0) sprintf(str, "%.1f(%.1f)", radar_dist, vision_dist);
-            else sprintf(str, "%.0f(%.1f)", radar_dist, vision_dist);
+            else sprintf(str, "%.0f(%.0f)", radar_dist, vision_dist);
             ui_draw_text(s, x, disp_y, str, disp_size, COLOR_WHITE, BOLD);
         }
     }
