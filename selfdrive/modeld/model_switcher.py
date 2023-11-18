229f157abe60a777e9f402b4f7dd1cb523824e59
ajouatom <ajouatom@gmail.com>
2023-11-18 12:17:55 +0900

    fix.. models.

--- selfdrive/modeld/model_switcher.py
+++ selfdrive/modeld/model_switcher.py
@@ -10,8 +10,9 @@ MODELS_SOURCE = os.path.join(DESTINATION_PATH, "models")
 THNEED_FILE = os.path.join(DESTINATION_PATH, "supercombo.thneed")
 
 MODEL_NAME = {
-  0: "bddriving",
+  0: "farmville",
   1: "new-lemon-pie",
+  2: "bddriving",
 }
 
 def set_model_list_parameter(params):
