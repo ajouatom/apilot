d2e7638da61a98f20025ddd07e9855c4e3633fd3
ajouatom <ajouatom@gmail.com>
2023-11-20 18:56:07 +0900

    test

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -289,8 +289,7 @@ def get_RadarState_from_vision(lead_msg: capnp._DynamicStructReader, v_ego: floa
 def get_lead(v_ego: float, ready: bool, tracks: Dict[int, Track], lead_msg: capnp._DynamicStructReader,
              model_v_ego: float, low_speed_override: bool = True, mixRadarInfo=0) -> Dict[str, Any]:
   # Determine leads, this is where the essential logic happens
-  #if len(tracks) > 0 and ready and lead_msg.prob > .5:
-  if len(tracks) > 0 and True and lead_msg.prob > .5:
+  if len(tracks) > 0:# and ready and lead_msg.prob > .5:
     track = match_vision_to_track(v_ego, lead_msg, tracks)
   else:
     track = None
