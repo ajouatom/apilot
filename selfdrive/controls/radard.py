d38ec1407b1495746ed471f5a23836f4b0adc6c5
ajouatom <ajouatom@gmail.com>
2023-11-20 13:56:23 +0900

    fix.. radard, y_sane (비전감지y와 레이더y가 많이 차이나면 삭제)

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -182,11 +182,19 @@ def match_vision_to_track(v_ego: float, lead: capnp._DynamicStructReader, tracks
 
   track = max(tracks.values(), key=prob)
 
+  #prob_values = {track_id: prob(track) for track_id, track in tracks.items()}
+  #max_prob_track_id = max(prob_values, key=prob_values.get)
+  #max_prob_value = prob_values[max_prob_track_id]
+  #track = tracks.get(max_prob_track_id)
+  # yRel값은 왼쪽이 +값, lead.y[0]값은 왼쪽이 -값
+
   # if no 'sane' match is found return -1
   # stationary radar points can be false positives
   dist_sane = abs(track.dRel - offset_vision_dist) < max([(offset_vision_dist)*.35, 5.0])
   vel_sane = (abs(track.vRel + v_ego - lead.v[0]) < 15) or (v_ego + track.vRel > 3)
-  if dist_sane and vel_sane:
+  ##간혹 어수선한경우, 전방에 차가 없지만, 좌우에 차가많은경우 억지로 레이더를 가져오는 경우가 있음..(레이더트랙의 경우)
+  y_sane = (abs(-lead.y[0]-track.yRel) < 3.2 / 2.)  #lane_width assumed 3.2M, laplacian_pdf 의 prob값을 검증하려했지만, y값으로 처리해도 될듯함.
+  if dist_sane and vel_sane and y_sane:
     return track
   else:
     return None
