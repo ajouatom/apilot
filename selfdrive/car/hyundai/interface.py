d96573fa78c97d629e084207c55ce248c9bf728f
ajouatom <ajouatom@gmail.com>
2023-11-22 16:24:25 +0900

    revert 20hz...: radard타이밍이 20hz로 안됨... 다른방법을...

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -367,8 +367,7 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalActuatorDelayLowerBound = 0.5
     ret.longitudinalActuatorDelayUpperBound = 0.5
 
-    #ret.radarTimeStep = (1.0 / 50) # 50Hz   SCC11, RadarTrack은 50Hz
-    ret.radarTimeStep = (1.0 / 20) # 20Hz 시험
+    ret.radarTimeStep = (1.0 / 50) # 50Hz   SCC11, RadarTrack은 50Hz
 
     # *** feature detection ***
     if candidate in CANFD_CAR:
