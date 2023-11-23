0cd4072319e359220e6161db2cd6d7365e728da4
ajouatom <43668841+ajouatom@users.noreply.github.com>
2023-11-23 09:04:45 +0900

    reset kalman: d_rel>3.0

--- selfdrive/controls/lib/radar_helpers.py
+++ selfdrive/controls/lib/radar_helpers.py
@@ -23,11 +23,11 @@ class Track():
     self.K_C = kalman_params.C
     self.K_K = kalman_params.K
     self.kf = KF1D([[v_lead], [0.0]], self.K_A, self.K_C, self.K_K)
-    self.vLead = v_lead
+    self.dRel = 0.0
 
   def update(self, d_rel, y_rel, v_rel, v_lead, measured):
     #apilot: changed radar target
-    if abs(self.vLead - v_lead) > 0.5:
+    if abs(self.dRel - d_rel) > 3.0:
       self.cnt = 0
       self.kf = KF1D([[v_lead], [0.0]], self.K_A, self.K_C, self.K_K)
 
